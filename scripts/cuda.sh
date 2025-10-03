9be1c7233a04a76e29b14f5088beafe54b5a7811
maybetree <maybetree48@proton.me>
2025-10-03 21:48:31 +0200

    typo

--- scripts/cuda.sh
+++ scripts/cuda.sh
@@ -14,10 +14,8 @@ then
 	else
 		echo "cuda-ubuntu2004.pin not in annex, downloading..."
 		wget -O ./depget/cuda-ubuntu2004.pin https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-		cp -L ./depget/cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 	fi
 else
-	cp -L ./dep/cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 fi
 
 if ! [ -f ./depget/cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb ]
@@ -33,6 +31,7 @@ then
 	fi
 fi
 
+cp -L ./depget/cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
 dpkg -i ./depget/cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb
 apt-key add /var/cuda-repo-ubuntu2004-11-0-local/7fa2af80.pub
 apt-get update
