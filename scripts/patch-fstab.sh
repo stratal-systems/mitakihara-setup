a935c2ec3aefc5c2bace9d1de4aecc199e3642f3
maybetree <maybetree48@proton.me>
2025-10-03 20:48:28 +0200

    typo

--- scripts/patch-fstab.sh
+++ scripts/patch-fstab.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-sed -i /etf/fstab 's|/ btrfs defaults|/ btrfs rw,noatime,lazytime,compress=zstd:12,ssd,discard=async,space_cache=v2,autodefrag,subvolid=5,subvol=/|'
+sed -i 's|/ btrfs defaults|/ btrfs rw,noatime,lazytime,compress=zstd:12,ssd,discard=async,space_cache=v2,autodefrag,subvolid=5,subvol=/|' /etc/fstab
 
 
