531a330936e589f728d4d47a95a5b86ea2ab6807
maybetree <maybetree48@proton.me>
2025-10-03 21:48:47 +0200

    typo

--- scripts/cuda.sh
+++ scripts/cuda.sh
@@ -15,7 +15,6 @@ then
 		echo "cuda-ubuntu2004.pin not in annex, downloading..."
 		wget -O ./depget/cuda-ubuntu2004.pin https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
 	fi
-else
 fi
 
 if ! [ -f ./depget/cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb ]
