1836572bd519042483199ec446e2c5ac207ef9a9
maybetree <maybetree48@proton.me>
2025-10-04 19:21:33 +0200

    autocommit Sat Oct  4 19:21:33 CEST 2025

--- scripts/setup-podman.sh
+++ scripts/setup-podman.sh
@@ -3,13 +3,24 @@
 # Thanks
 # https://www.atlantic.net/dedicated-server-hosting/how-to-install-and-use-podman-on-ubuntu/
 
-apt-get install curl wget gnupg2 -y
+if ! command -v podman
+then
+	echo "Podman command not found, installing..."
 
-source /etc/os-release
-sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+	set -e
 
-wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | apt-key add -
+	apt-get install curl wget gnupg2 -y
 
-apt-get update -qq -y
-apt-get -qq --yes install podman
+	source /etc/os-release
+	sh -c "echo 'deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /' > /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list"
+
+	wget -nv https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_${VERSION_ID}/Release.key -O- | apt-key add -
+
+	apt-get update -qq -y
+	apt-get -qq --yes install podman
+
+	set +e
+else
+	echo "podman command found, skipping install"
+fi
 
