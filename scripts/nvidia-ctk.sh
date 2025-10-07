00f8e94e4a3e4b2b1a9afec289c624473b8b5f02
maybetree <maybetree48@proton.me>
2025-10-07 21:06:35 +0200

    autocommit Tue Oct  7 21:06:35 CEST 2025

--- scripts/nvidia-ctk.sh
+++ scripts/nvidia-ctk.sh
@@ -1,10 +1,22 @@
 #!/bin/sh
 
 # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
-#
+
+if command -v nvidia-ctk
+then
+	echo "nvidia-ctk command present, bye!"
+	exit 2
+fi
+
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey |
 	gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
 	&& curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
 	sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
 	sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+apt update
+apt install \
+	libnvidia-container-tools \
+	nvidia-container-toolkit \
+	nvidia-container-toolkit-base
 
