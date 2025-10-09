9fcb5bc811d072b3e2c364fbb7e902b6bcbc3b83
maybetree <maybetree48@proton.me>
2025-10-09 16:05:13 +0200

    autocommit Thu Oct  9 16:05:13 CEST 2025

--- scripts/gpg-add-key.sh
+++ scripts/gpg-add-key.sh
@@ -5,6 +5,8 @@ set -e
 test -r dep/stratal-systems.gpg
 pubkey="$(cat dep/stratal-systems.gpg)"
 cd ~/srv/mitakihara
+podman-compose exec -T minicycle mkdir /root/.gnupg
+podman-compose exec -T minicycle chmod 700 /root/.gnupg
 echo "$pubkey" |
-	podman-compose exec -ti minicycle gpg --import
+	podman-compose exec -T minicycle gpg --import
 
