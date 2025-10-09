3d2d1c105ee1b8130fdf89f5220501379335c0d2
maybetree <maybetree48@proton.me>
2025-10-09 16:00:46 +0200

    autocommit Thu Oct  9 16:00:46 CEST 2025

--- scripts/gpg-add-key.sh
+++ scripts/gpg-add-key.sh
@@ -3,7 +3,7 @@
 set -e
 
 test -r dep/stratal-systems.gpg
-pubkey="$(dep/stratal-systems.gpg)"
+pubkey="$(cat dep/stratal-systems.gpg)"
 cd ~/srv/mitakihara
 echo "$pubkey" |
 	podman-compose exec minicycle gpg --import
