b6ee8e17dfaa31fc712ea5bc965260213a2eec4e
maybetree <maybetree48@proton.me>
2025-10-11 15:01:08 +0200

    add setup script

--- setup.sh
+++ setup.sh
@@ -1,2 +1,13 @@
 #!/bin/sh
 
+set -e
+
+git pull origin
+git annex sync --no-push origin
+git annex get .
+
+./scritps/mod-apply.sh maybetree-compose
+./scritps/mod-apply.sh maybetree-compose-secret
+sudo ./scritps/mod-apply.sh root-compose
+sudo ./scritps/mod-apply.sh root-compose-secret
+
