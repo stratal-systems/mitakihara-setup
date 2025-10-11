78b9d36484898d83d2a6b7f103a33c7aa2e64f92
maybetree <maybetree48@proton.me>
2025-10-12 01:51:01 +0200

    autocommit Sun Oct 12 01:51:01 CEST 2025

--- scripts/cuda.sh
+++ scripts/cuda.sh
@@ -1,40 +1,22 @@
 #!/bin/bash
 
+pinfile="cuda-ubuntu2004.pin"
+debfile="cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb"
+
 set -e
 
 mkdir -p ./depget
 
-if ! [ -f ./depget/cuda-ubuntu2004.pin ]
-then
-	echo "cuda-ubuntu2004.pin not found"
-	if [ -r "./dep/cuda-ubuntu2004.pin" ]
-	then
-		echo "cuda-ubuntu2004.pin in annex"
-		cp -L /etc/apt/preferences.d/cuda-repository-pin-600 ./dep/cuda-ubuntu2004.pin 
-	else
-		echo "cuda-ubuntu2004.pin not in annex, downloading..."
-		wget -O ./depget/cuda-ubuntu2004.pin https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-	fi
-fi
+git annex get "./dep/$pinfile" "./dep/$debfile"
 
-if ! [ -f ./depget/cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb ]
-then
-	echo "cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb not found"
-	if [ -r "./dep/cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb" ]
-	then
-		echo "cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb in annex"
-		cp -L ./dep/cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb /etc/apt/preferences.d/cuda-repository-pin-600
-	else
-		echo "cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb not in annex, downloading..."
-		wget -O ./depget/cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb
-	fi
-fi
+# wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+# wget http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb
 
-cp -L ./depget/cuda-ubuntu2004.pin /etc/apt/preferences.d/cuda-repository-pin-600
-dpkg -i ./depget/cuda-repo-ubuntu2004-11-0-local_11.0.2-450.51.05-1_amd64.deb
-apt-key add /var/cuda-repo-ubuntu2004-11-0-local/7fa2af80.pub
-apt-get update
-apt-get -y install cuda
+sudo cp -L "./dep/$pinfile" /etc/apt/preferences.d/cuda-repository-pin-600
+sudo dpkg -i "./dep/$debfile"
+sudo apt-key add /var/cuda-repo-ubuntu2004-11-0-local/7fa2af80.pub
+sudo apt-get update
+sudo apt-get -y install cuda
 
 echo "It installed, disabling gdm service!"
 
